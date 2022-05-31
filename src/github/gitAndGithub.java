8f45a399f5b26326a9123b5901b800899fb1ed1b
osayla <osayla89@gmail.com>
2022-05-31 19:55:08 -0400

    2nd commit

--- src/github/gitAndGithub.java
+++ src/github/gitAndGithub.java
@@ -5,6 +5,7 @@ public class gitAndGithub {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("last class");
+System.out.println("today is 97 degrees");
 	}
 
 }
